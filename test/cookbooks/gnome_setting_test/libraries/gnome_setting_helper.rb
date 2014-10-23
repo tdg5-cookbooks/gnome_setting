7903b4f42b3b99163d88291352a2f281762ad4fa
Danny Guinther <dannyguinther@gmail.com>
2014-10-23 19:55:40 -0400

    Refactor and reduce integration test

--- test/cookbooks/gnome_setting_test/libraries/gnome_setting_helper.rb
+++ test/cookbooks/gnome_setting_test/libraries/gnome_setting_helper.rb
@@ -13,59 +13,26 @@ module GnomeSettingHelper
   # Settings to test with. The constant is self referential, so should probably
   # be defined last.
   SETTINGS = {
-    :favorite_apps => {
+    :schema_without_path_string => {
+      :key => 'session-name',
+      :schema => 'org.gnome.desktop.session',
+      :user => username,
+      :value => 'gnome',
+    },
+    :schema_without_path_array_of_strings => {
       :key => 'favorite-apps',
       :schema => 'org.gnome.shell',
       :user => username,
       :value => [
-        'google-chrome.desktop',
-        'pidgin.desktop',
-        'spotify.desktop',
-        'rhythmbox.desktop',
-        'nautilus.desktop',
-        'gnome-terminal.desktop',
-        'gimp.desktop',
-        'virtualbox.desktop',
         'firefox.desktop',
+        'gimp.desktop',
+        'gnome-terminal.desktop',
+        'nautilus.desktop',
+        'pidgin.desktop',
+        'rhythmbox.desktop',
       ],
     },
-    :move_to_workspace_down_keybindings => {
-      :key => 'move-to-workspace-down',
-      :schema => 'org.gnome.desktop.wm.keybindings',
-      :user => username,
-      :value => [
-        '<Control><Shift><Alt>Down',
-        '<Control><Shift><Alt>j',
-      ],
-    },
-    :move_to_workspace_up_keybindings => {
-      :key => 'move-to-workspace-up',
-      :schema => 'org.gnome.desktop.wm.keybindings',
-      :user => username,
-      :value => [
-        '<Control><Shift><Alt>Up',
-        '<Control><Shift><Alt>k',
-      ],
-    },
-    :switch_to_workspace_up_keybinding => {
-      :key => 'switch-to-workspace-up',
-      :schema => 'org.gnome.desktop.wm.keybindings',
-      :user => username,
-      :value => [
-        '<Control><Alt>Up',
-        '<Control><Alt>k',
-      ],
-    },
-    :switch_to_workspace_down_keybinding => {
-      :key => 'switch-to-workspace-down',
-      :schema => 'org.gnome.desktop.wm.keybindings',
-      :user => username,
-      :value => [
-        '<Control><Alt>Down',
-        '<Control><Alt>j',
-      ],
-    },
-    :launch_terminal_keybinding_name => {
+    :schema_with_path_string => {
       :key => 'name',
       :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/',
       :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
@@ -73,81 +40,11 @@ module GnomeSettingHelper
       :value => 'Launch Terminal',
     },
     :launch_terminal_keybinding_command => {
-      :key => 'command',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
+      :key => 'application-id',
+      :path => '/',
+      :schema => 'org.gnome.desktop.notifications.application',
       :user => username,
-      :value => 'x-terminal-emulator',
-    },
-    :launch_terminal_keybinding_binding => {
-      :key => 'binding',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => '<Primary><Alt>t',
-    },
-    :launch_chrome_keybinding_name => {
-      :key => 'name',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'Launch Chrome',
-    },
-    :launch_chrome_keybinding_command => {
-      :key => 'command',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'new_chrome_with_focus',
-    },
-    :launch_chrome_keybinding_binding => {
-      :key => 'binding',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => '<Primary><Alt>b',
-    },
-    :launch_chrome_incognito_name => {
-      :key => 'name',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'Launch Chrome Incognito',
-    },
-    :launch_chrome_incognito_command => {
-      :key => 'command',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'new_chrome_with_focus --incognito',
-    },
-    :launch_chrome_incognito_binding => {
-      :key => 'binding',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => '<Primary><Shift><Alt>b',
-    },
-    :suspend_keybinding_name => {
-      :key => 'name',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'Suspend',
-    },
-    :suspend_keybinding_command => {
-      :key => 'command',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => 'dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend',
-    },
-    :suspend_keybinding_binding => {
-      :key => 'binding',
-      :path => '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/',
-      :schema => 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding',
-      :user => username,
-      :value => '<Primary><Shift><Alt>s',
+      :value => 'alert',
     },
   }.freeze
 
--- test/integration/gnome_setting/serverspec/set_spec.rb
+++ test/integration/gnome_setting/serverspec/set_spec.rb
@@ -5,17 +5,39 @@ set :backend, :exec
 
 describe 'gnome_setting' do
 
-  it 'sets all settings correctly' do
-    GnomeSettingHelper.settings.each do |name, setting|
-      gsetting = Gnome::Setting.new(setting[:user], setting[:schema], setting[:path])
-      expected_value = format_as_serialized_gvariant(setting[:value])
-      value_found = gsetting.get(setting[:key], setting[:path])
-      expect(value_found).to eql(expected_value)
+  describe 'a schema without a path' do
+
+    it 'sets a key with a String datatype' do
+      test_setting(GnomeSettingHelper.settings[:schema_without_path_array_of_strings])
     end
+
+
+    it 'sets a key with an Array<String> datatype' do
+      test_setting(GnomeSettingHelper.settings[:schema_without_path_string])
+    end
+
   end
+
+
+  describe 'a schema with a path' do
+
+    it 'sets a key with a String datatype' do
+      test_setting(GnomeSettingHelper.settings[:schema_with_path_string])
+    end
+
+  end
+
 
   def format_as_serialized_gvariant(value)
     value.inspect.gsub(/"/, "'")
+  end
+
+
+  def test_setting(setting)
+    gsetting = Gnome::Setting.new(setting[:user], setting[:schema], setting[:path])
+    expected_value = format_as_serialized_gvariant(setting[:value])
+    found = gsetting.get(setting[:key], setting[:path])
+    expect(found).to eql(expected_value)
   end
 
 end
