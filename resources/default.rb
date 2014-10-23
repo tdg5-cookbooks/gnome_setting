3331b2048c2c4a537829540fba94bd0764ce81ca
Danny Guinther <dannyguinther@gmail.com>
2014-10-23 08:45:48 -0400

    Remove antiquated type attribute

--- resources/default.rb
+++ resources/default.rb
@@ -4,6 +4,5 @@ attribute :key, :kind_of => String, :required => true
 attribute :name, :kind_of => String, :name_attribute => true, :required => true
 attribute :path, :kind_of => String
 attribute :schema, :kind_of => String, :required => true
-attribute :type, :equal_to => [:String, :StringList], :required => true
 attribute :user, :kind_of => String, :required => true
 attribute :value, :kind_of => [Array, String], :required => true
